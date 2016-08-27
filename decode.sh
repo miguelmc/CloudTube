a5d8a5b86b2029a87f6c6129fe4bed13ebc9b727
miguelmc <miguelmtz93@gmail.com>
2016-08-27 13:55:26 -0500

    Initial commit

---
+++ decode.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+ffmpeg -i "$1" -r 1 -f rawvideo temp
+cat temp | ./lvdo/src/lvdodec -s 640x480 -q 6 --qmin 1 --qmax 4 | cat > end_result
+rm temp
---
+++ pipeline.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+$PWD/encode.sh $1
+$PWD/decode.sh fileout.mp4
