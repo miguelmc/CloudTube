e811a8f49efd0dc9711e94f83e3e579387cdeb0b
miguelmc <miguelmtz93@gmail.com>
2016-08-28 12:26:56 -0500

    Dogeee

--- rsencode.sh
+++ rsencode.sh
@@ -2,4 +2,9 @@
 
 # Reed solomon algorithm
 
+# Many math
+#                   many algorithms
+#     much shibe
+#                                much wow
+
 cat $1 | ./ezpwd-reed-solomon/rsencode_16 > uno 
