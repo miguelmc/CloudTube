0cc0a19f0f185fe8f011e6d8a677b17e3127e8f7
miguelmc <miguelmtz93@gmail.com>
2016-08-28 03:08:52 -0500

    ahi vamos

--- split_upload.py
+++ split_upload.py
@@ -21,19 +21,27 @@ def main(argv):
                 fullname = os.path.basename(filename) + str(i)
                 byte = f.read(65536)
 
-                outfile = open(fullname, "wb")
 
                 if byte == "":
                     break
+
+                outfile = open(fullname, "wb")
+                
                 if len(byte) % 2 == 1:
                     byte += '0'
 
                 outfile.write(byte)
                 outfile.close()
 
-                subprocess.call("cat " + fullname + " |  ./ezpwd-reed-solomon/rsencode_16 > rs" + str(i) + fullname, shell=True)
+                # Do rsencode
+                subprocess.call("cat " + fullname + " |  ./ezpwd-reed-solomon/rsencode_16 > rs" + fullname, shell=True)
+                subprocess.call("rm " + fullname, shell=True)
 
-                print i
+                rsname = "rs" + fullname
+
+                subprocess.call("cat " + rsname + " | ./lvdo/src/lvdoenc -s 640x480 -q 6 --qmin 1 --qmax 4 | x264 --input-res 640x480 --fps 1 --profile high --level 5.1 --tune stillimage --crf 22 --colormatrix bt709 --me dia --merange 0 -o " + fullname + ".mp4 -", shell=True)
+                subprocess.call("rm " + rsname, shell=True)
+
                 i += 1
 
 
